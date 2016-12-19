f1cab6d13f6b44bbc8ef1de7fec40f7850a1f383
Dani Adkins <danirenaeadkins12@gmail.com>
2016-12-19 17:14:16 -0600

    added edit modal

--- javascripts/controllers/MyPostsCtrl.js
+++ javascripts/controllers/MyPostsCtrl.js
@@ -1,11 +1,16 @@
 "use strict";
 
-app.controller("MyPostsCtrl", function($scope, SearchFactory, PostFactory, $rootScope){
+app.controller("MyPostsCtrl", function($scope, SearchFactory, PostFactory, $rootScope, $location){
 
 let userId = $rootScope.user.uid;
 $scope.myPost = {};
 let allPosts = [];
 $scope.posts = [];
+$scope.selected = {};
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
 
  let getPosts = function () {
  PostFactory.getPostByUid(userId).then((postByUid)=>{
@@ -23,11 +28,38 @@ $scope.deletePost = function(postId){ //passed this in on click here and in HTML
   });
 };
 
-// edit function from todo - also look at Edit controller and edit in factory
-$scope.inputChange = function(thingy){
+$scope.selectedPost = function(thisSelected){
+  $scope.selected = thisSelected;
+}
 
-  PostFactory.editPost(thingy).then(function(response){
-    getPosts();
+// edit function from todo - also look at Edit controller and edit in factory
+// $scope.inputChange = function(thingy){
+//   PostFactory.editPost(thingy).then(function(response){
+//     getPosts();
+//   });
+
+
+
+  // $scope.updatedPost = {}
+
+$scope.submitUpdatedPost = (submitUpdated)=>{
+  console.log("SUBMIT HAPPENED", submitUpdated);
+  // $scope.updatedPost.uid = $rootScope.user.uid;
+      // $scope.updatedPost.airportCode = $scope.searchedAirportCode.code;
+      // $scope.updatedPost.airportCity = $scope.searchedAirportCode.city;
+      // $scope.updatedPost.username = $rootScope.user.username;
+      // $scope.updatedPost.timeStamp = new Date();
+      // $scope.updatedPost.comment = {};
+      // $scope
+      //how can I implement this?
+      // moment.format('MMMM Do YYYY, h:mm:ss a');
+  PostFactory.editPost($scope.selectedPost).then(function(postId){
+    // $scope.updatedPost = {};
+    $location.url("/my-posts")
   });
 };
 });
+
+
+
+
